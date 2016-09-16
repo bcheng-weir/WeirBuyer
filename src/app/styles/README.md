0c81505debee1c83155cda9ac3c6cb361c2e26ce
rescht <reschtij@gmail.com>
2016-09-16 16:00:14 -0400

    Moved styles examples to their own section, leave empty home component for Buyer App landing page from POC.

---
+++ src/app/styles/README.md
@@ -0,0 +1,5 @@
+# The `src/app/styles` Directory
+
+## Overview
+
+This component was the default home pages, and provides a place to view the current styles for the application.
---
+++ src/app/styles/styles.js
@@ -0,0 +1,20 @@
+angular.module('orderCloud')
+	.config(StylesConfig)
+	.controller('StylesCtrl', StylesController)
+;
+
+function StylesConfig($stateProvider) {
+	$stateProvider
+		.state('styles', {
+			parent: 'base',
+			url: '/styles',
+			templateUrl: 'styles/templates/styles.tpl.html',
+			controller: 'StylesCtrl',
+			controllerAs: 'styles'
+		})
+	;
+}
+
+function StylesController() {
+	var vm = this;
+}

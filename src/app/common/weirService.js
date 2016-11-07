ab6ae404d86fa9baa1f66ce746533ac7ead52c73
Dave Ferguson <davfergus@gmail.com>
2016-11-07 12:45:38 -0600

    Updated weir service statuses.

--- src/app/common/weirService.js
+++ src/app/common/weirService.js
@@ -3,26 +3,33 @@ angular.module( 'orderCloud' )
 ;
 
 function WeirService( $q, $cookieStore, $sce, $exceptionHandler, OrderCloud, CurrentOrder, Underscore, buyerid ) {
-    var orderStatuses = {
-	    Draft: {id: "DR", label: "Draft", desc: "This is the current quote under construction"},
-	    Saved: {id: "SV", label: "Saved", desc: "Quote has been saved but not yet shared"},
-	    Shared: {id: "SH", label: "Shared", desc: "Shopper quote has been shared with a buyer"},
-	    Approved: {id: "AP", label: "Approved", desc: "Shopper quote has been shared with a buyer and approved"},
-        Rejected: {id: "RJ", label: "Rejected", desc: "Shopper quote has been shared with a buyer and then rejected"},
-        Submitted: {id: "SB", label: "Submitted", desc: "Quote has been submitted to Weir"},
-        SubmittedWithPO: {id: "SP", label: "Order submitted with PO", desc: "Order has been submitted to Weir with a PO"},
-        ConfirmedPending: {id: "CP", label: "Confirmed Quote", desc: "Quote has been submitted and confirmed by Weir, pending addition of PO number"},
-        Review: {id: "RV", label: "Under review", desc: "Order has been submitted to Weir, but a change or additional information is needed"},
-        Confirmed: {id: "CF", label: "Confirmed", desc: "Order has been submitted to and confirmed by Weir, and PO number is attached"},
-        Cancelled: {id: "CX", label: "Cancelled", desc: "Order cancelled after submission"},
-        Despatched: {id: "DP", label: "Despatched", desc: "Order marked as despatched"},
-        Invoiced: {id: "IV", label: "Invoiced", desc: "Order marked as invoiced"}
-    };
-    var orderStatusList = [
-	    orderStatuses.Draft, orderStatuses.Saved, orderStatuses.Shared, orderStatuses.Approved, orderStatuses.Rejected,
-	    orderStatuses.Submitted, orderStatuses.ConfirmedPending, orderStatuses.SubmittedWithPO, orderStatuses.Review,
-	    orderStatuses.Confirmed, orderStatuses.Cancelled, orderStatuses.Despatched, orderStatuses.Invoiced
-    ];
+	var orderStatuses = {
+		Draft: {id: "DR", label: "Draft", desc: "This is the current quote under construction"},
+		Saved: {id: "SV", label: "Saved", desc: "Quote has been saved but not yet submitted to weir as quote or order"},
+		Submitted: {id:"SB", label:"Quote Submitted for Review", desc:"Customer has selected to request review OR review status is conditional based on POA items being included in quote"},
+		RevisedQuote: {id:"RV", label:"Revised Quote", desc:"Weir have reviewed the quote and updated items as required. When the update is saved and ‘shared with the customer this becomes a ‘Revised quote’"},
+		RejectedQuote: {id: "RQ", label: "Rejected Quote", desc: "Weir have shared Revised quote with buyer has rejected revision (this would display as a status in the list view of quotes rather than in the navigation)"},
+		ConfirmedQuote: {id: "CQ", label: "Confirmed Quote", desc: "1. Customer has approved revised quote – assumes that if Weir have updated a revised quote the new /revised items are ‘pre-approved’ by Weir. 2. Weir admin has  confirmed a quote submitted for review by the customer."},
+		SubmittedWithPO: {id: "SP", label: "Order submitted with PO", desc: "Order has been submitted to Weir with a PO"},
+		RevisedOrder: {id: "RO", label: "Revised Order", desc: "1. Weir have reviewed the order and updated items as required. When the update is saved and ‘shared with the customer this becomes a ‘Revised Order’."},
+		RejectedRevisedOrder: {id: "RR", label: "Rejected Revised Order", desc: "Weir have shared revised order and customer has rejected revision (this would display as a status in the list view of quotes rather than in the navigation)"},
+		ConfirmedOrder: {id: "CO", label: "Confirmed Order", desc: "1, Weir have reviewed order and confirmed all details are OK 2, Customer has accepted revised order"},
+		Despatched: {id: "DP", label: "Despatched", desc: "Order marked as despatched"},
+		Invoiced: {id: "IV", label: "Invoiced", desc: "Order marked as invoiced"}
+		/*Shared: {id: "SH", label: "Shared", desc: "Shopper quote has been shared with a buyer"}, //Should this be an XP?
+		 Approved: {id: "AP", label: "Approved", desc: "Shopper quote has been shared with a buyer and approved"},
+		 Rejected: {id: "RJ", label: "Rejected", desc: "Shopper quote has been shared with a buyer and then rejected"},
+		 Submitted: {id: "SB", label: "Submitted", desc: "Customer has selected to request review OR review status is conditional based on POA items being included in quote"},
+		 ConfirmedPending: {id: "CP", label: "Confirmed Quote", desc: "Quote has been submitted and confirmed by Weir, pending addition of PO number"},
+		 Review: {id: "RV", label: "Under review", desc: "Order has been submitted to Weir, but a change or additional information is needed"},
+		 Confirmed: {id: "CF", label: "Confirmed", desc: "Order has been submitted to and confirmed by Weir, and PO number is attached"},
+		 Cancelled: {id: "CX", label: "Cancelled", desc: "Order cancelled after submission"},*/
+	};
+	var orderStatusList = [
+		orderStatuses.Draft, orderStatuses.Saved, orderStatuses.Submitted, orderStatuses.RevisedQuote,
+		orderStatuses.RejectedQuote, orderStatuses.ConfirmedQuote, orderStatuses.SubmittedWithPO, orderStatuses.RevisedOrder,
+		orderStatuses.RejectedRevisedOrder, orderStatuses.ConfirmedOrder, orderStatuses.Despatched, orderStatuses.Invoiced
+	];
     // TODO - add localized label/description, include locale in selection
     function getStatus(id) {
 	var match = null;

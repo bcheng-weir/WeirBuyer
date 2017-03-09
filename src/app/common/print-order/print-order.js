080a2de291c252d6b0c35eaa4f528083c3b648fa
Dave Ferguson <davfergus@gmail.com>
2017-03-09 15:25:25 -0600

    FR print form updated.

--- src/app/common/print-order/print-order.js
+++ src/app/common/print-order/print-order.js
@@ -3,7 +3,7 @@ angular.module('orderCloud')
 	.controller('printOrderBtnCtrl',PrintOrderButtonControl)
 	.directive('printOrderButton',PrintOrderButtonDirective);
 
-function PrintOrderController(printData,$timeout,$window,WeirService) {
+function PrintOrderController(printData,$timeout,$window,WeirService,$sce) {
 	var vm = this;
 	// ToDo Get the catalog (UK or FR) and buyer (WVCUIK-1352) data.
 	vm.buyer = printData.buyer;
@@ -12,21 +12,49 @@ function PrintOrderController(printData,$timeout,$window,WeirService) {
 	vm.address = printData.address;
 	vm.pocontent = printData.pocontent;
 	var labels = {
-		en: {},
-		fr: {}
+		en: {
+			QuoteNumber: "Quote Number; ",
+			YourReference: "Your Reference No; ",
+			PONumber: "PO Number;",
+			SerialNum: "Serial Number",
+			TagNum: "Tag Number (if available)",
+			PartNum: "Part Number",
+			PartDesc: "Description of Part",
+			RecRepl: "Recommended Replacement (yrs)",
+			LeadTime: "Lead Time (days)",
+			PricePer: "Price per Item or Set",
+			Quantity: "Quantity",
+			Total: "Total",
+			DeliveryAddress: "Delivery Address"
+		},
+		fr: {
+			QuoteNumber: $sce.trustAsHtml("Num&eacute;ro de cotation "),
+			YourReference: $sce.trustAsHtml("Votre num&eacute;ro de r&eacute;f&eacute;rence; "),
+			PONumber: $sce.trustAsHtml("Numéro de bon de commande;"),
+			SerialNum: $sce.trustAsHtml("Num&eacute;ro de S&eacute;rie"),
+			TagNum: $sce.trustAsHtml("Numéro de repère soupape"),
+			PartNum: $sce.trustAsHtml("R&eacute;f&eacute;rence de la pi&egrave;ce"),
+			PartDesc: $sce.trustAsHtml("Description de la pi&egrave;ce"),
+			RecRepl: $sce.trustAsHtml("Remplacement recommand&eacute; (ans)"),
+			LeadTime: $sce.trustAsHtml("D&eacute;lai de livraison (journées)"),
+			PricePer: $sce.trustAsHtml("Prix par item ou par kit"),
+			Quantity: $sce.trustAsHtml("Quantit&eacute;"),
+			Total: $sce.trustAsHtml("Total"),
+			DeliveryAddress: $sce.trustAsHtml("Adresse de livraison")
+		}
 	};
 	vm.labels = labels[WeirService.Locale()];
 	$timeout($window.print,1);
 }
 
-function PrintOrderButtonControl($scope,imageRoot,WeirService,$uibModal) {
+function PrintOrderButtonControl($scope,imageRoot,WeirService,$uibModal,$sce) {
 	var vm = this;
 	var labels = {
 		en: {
 			print:'Print'
 		},
 		fr: {
-			print:'Print'
+			print: $sce.trustAsHtml("Imprimer")
 		}
 	};
 	vm.labels = labels[WeirService.Locale()];

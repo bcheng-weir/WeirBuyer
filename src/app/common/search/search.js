6e1c9f73c9d07cddabefbefc241972da23d2df88
Dave Ferguson <davfergus@gmail.com>
2017-03-29 08:33:57 -0500

    Reverting search. Updates were to be in dev, not QA.

--- src/app/common/search/search.js
+++ src/app/common/search/search.js
@@ -1,194 +1,187 @@
 angular.module('ordercloud-search', []);
 
 angular.module('ordercloud-search')
-    .directive('ordercloudSearch', OrdercloudSearch)
-    .controller('ordercloudSearchCtrl', OrdercloudSearchController)
-    .factory('TrackSearch', TrackSearchService )
-    .factory('SearchProducts', SearchProductsService)
+	.directive('ordercloudSearch', OrdercloudSearch)
+	.controller('ordercloudSearchCtrl', OrdercloudSearchController)
+	.factory('TrackSearch', TrackSearchService )
+	.factory('SearchProducts', SearchProductsService)
 ;
 
 function OrdercloudSearch () {
-    return {
-        scope: {
-            placeholder: '@',
-            servicename: '@',
-            controlleras: '='
-        },
-        restrict: 'E',
-        templateUrl: 'common/search/templates/search.tpl.html',
-        controller: 'ordercloudSearchCtrl',
-        controllerAs: 'ocSearch',
-        replace: true
-    }
+	return {
+		scope: {
+			placeholder: '@',
+			servicename: '@',
+			controlleras: '='
+		},
+		restrict: 'E',
+		templateUrl: 'common/search/templates/search.tpl.html',
+		controller: 'ordercloudSearchCtrl',
+		controllerAs: 'ocSearch',
+		replace: true
+	}
 }
 
 function OrdercloudSearchController($timeout, $scope, OrderCloud, TrackSearch) {
-    $scope.searchTerm = null;
-    if ($scope.servicename) {
-        var var_name = $scope.servicename.replace(/([a-z])([A-Z])/g, '$1 $2');
-        $scope.placeholder = "Search " + var_name + '...';
-        var Service = OrderCloud[$scope.servicename];
-    }
-    var searching;
-    $scope.$watch('searchTerm', function(n,o) {
-        if (n == o) {
-            if (searching) $timeout.cancel(searching);
-        } else {
-            if (searching) $timeout.cancel(searching);
-            searching = $timeout(function() {
-                n == '' ? n = null : angular.noop();
-                TrackSearch.SetTerm(n);
-                if($scope.servicename === 'Orders') {
-                    if (!$scope.controlleras.searchfunction) {
-                        Service.ListIncoming(null, null, n)
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                    else {
-                        $scope.controlleras.searchfunction($scope.searchTerm)
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                }
-                else if ($scope.servicename === 'SpendingAccounts') {
-                    if (!$scope.controlleras.searchfunction) {
-                        Service.List(n, null, null, null, null, {'RedemptionCode': '!*'})
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                    else {
-                        $scope.controlleras.searchfunction($scope.searchTerm)
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                }
-                else if ($scope.servicename === 'Shipments') {
-                    if (!$scope.controlleras.searchfunction) {
-                        Service.List(null, n, null, null)
-                            .then(function (data) {
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                    else {
-                        $scope.controlleras.searchfunction($scope.searchTerm)
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                }
-                else {
-                    if (!$scope.controlleras.searchfunction) {
-                        Service.List(n)
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                    else {
-                        $scope.controlleras.searchfunction($scope.searchTerm)
-                            .then(function (data){
-                                $scope.controlleras.list = data;
-                            });
-                    }
-                }
+	$scope.searchTerm = null;
+	if ($scope.servicename) {
+		var var_name = $scope.servicename.replace(/([a-z])([A-Z])/g, '$1 $2');
+		$scope.placeholder = "Search " + var_name + '...';
+		var Service = OrderCloud[$scope.servicename];
+	}
+	var searching;
+	$scope.$watch('searchTerm', function(n,o) {
+		if (n == o) {
+			if (searching) $timeout.cancel(searching);
+		} else {
+			if (searching) $timeout.cancel(searching);
+			searching = $timeout(function() {
+				n == '' ? n = null : angular.noop();
+				TrackSearch.SetTerm(n);
+				if($scope.servicename === 'Orders') {
+					if (!$scope.controlleras.searchfunction) {
+						Service.ListIncoming(null, null, n)
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+					else {
+						$scope.controlleras.searchfunction($scope.searchTerm)
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+				}
+				else if ($scope.servicename === 'SpendingAccounts') {
+					if (!$scope.controlleras.searchfunction) {
+						Service.List(n, null, null, null, null, {'RedemptionCode': '!*'})
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+					else {
+						$scope.controlleras.searchfunction($scope.searchTerm)
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+				}
+				else if ($scope.servicename === 'Shipments') {
+					if (!$scope.controlleras.searchfunction) {
+						Service.List(null, n, null, null)
+							.then(function (data) {
+								$scope.controlleras.list = data;
+							});
+					}
+					else {
+						$scope.controlleras.searchfunction($scope.searchTerm)
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+				}
+				else {
+					if (!$scope.controlleras.searchfunction) {
+						Service.List(n)
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+					else {
+						$scope.controlleras.searchfunction($scope.searchTerm)
+							.then(function (data){
+								$scope.controlleras.list = data;
+							});
+					}
+				}
 
-            }, 300);
-        }
-    });
+			}, 300);
+		}
+	});
 }
 
 function TrackSearchService() {
-    var service = {
-        SetTerm: _setTerm,
-        GetTerm: _getTerm
-    };
+	var service = {
+		SetTerm: _setTerm,
+		GetTerm: _getTerm
+	};
 
-    var term = null;
+	var term = null;
 
-    function _setTerm(value) {
-        term = value;
-    }
+	function _setTerm(value) {
+		term = value;
+	}
 
-    function _getTerm() {
-        return term;
-    }
+	function _getTerm() {
+		return term;
+	}
 
-    return service;
+	return service;
 }
 
 function SearchProductsService($q, OrderCloud, Me, SearchTypeService) {
 	//This service handles type ahead. for app/search and app/home.
-    var service = {
-        GetAllSerialNumbers: _getAllSerialNumbers,
-        GetAllTagNumbers: _getAllTagNumbers,
-        GetAllPartNumbers: _getAllPartNumbers,
-	    GetPart: _getPart
-    };
+	var service = {
+		GetAllSerialNumbers: _getAllSerialNumbers,
+		GetAllTagNumbers: _getAllTagNumbers,
+		GetAllPartNumbers: _getAllPartNumbers,
+		GetPart: _getPart
+	};
 	var partResults = {};
-    //First three are the home page search methods.
+	//First three are the home page search methods.
 	function _getAllSerialNumbers(lookForThisPartialSerialNumber) {
 		var dfd = $q.defer();
 		var filter = {
-			"xp.SN": lookForThisPartialSerialNumber + "*"
+			"xp.SN":lookForThisPartialSerialNumber+"*"
 		};
-		if (Me.Org.xp.WeirGroup.id == "1") { // No global search for UK.
+		if(Me.Org.xp.WeirGroup.id=="1") { // No global search for UK.
 			filter.ParentID = Me.Org.ID;
 		}
 
-		OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter, Me.Org.xp.WeirGroup.id == "1" ? null : "all", Me.Org.xp.WeirGroup.label)
+		OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter, Me.Org.xp.WeirGroup.id=="1" ? null : "all", Me.Org.xp.WeirGroup.label)
 			.then(function(response) {
-				OrderCloud.Me.ListCategories(lookForThisPartialSerialNumber, 1, 50, "Description", null, null, "all", Me.Org.xp.WeirGroup.label)
-					.then(function (responseDescription) {
-						var returnResults = response.Items.concat(responseDescription.Items);
-						returnResults = _.uniq(returnResults, false, function (cat) {
-							return cat.xp.SN
-						});
-						dfd.resolve(returnResults);
-					});
+				dfd.resolve(response.Items);
 			});
 		return dfd.promise;
 	}
 
-    function _getAllTagNumbers(lookForThisPartialTagNumber) {
-    	var dfd = $q.defer();
-	    var filter = {
-		    "xp.TagNumber":lookForThisPartialTagNumber+"*"
-	    };
-	    if(Me.Org.xp.WeirGroup.id=="1") { //No global search for UK
-		    filter.ParentID = Me.Org.ID;
-	    }
+	function _getAllTagNumbers(lookForThisPartialTagNumber) {
+		var dfd = $q.defer();
+		var filter = {
+			"xp.TagNumber":lookForThisPartialTagNumber+"*"
+		};
+		if(Me.Org.xp.WeirGroup.id=="1") { //No global search for UK
+			filter.ParentID = Me.Org.ID;
+		}
 
-        OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter, Me.Org.xp.WeirGroup.id=="1" ? null : "all", Me.Org.xp.WeirGroup.label)
-            .then(function(response) {
-            	dfd.resolve(response.Items);
-            });
-	    return dfd.promise;
-    }
+		OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter, Me.Org.xp.WeirGroup.id=="1" ? null : "all", Me.Org.xp.WeirGroup.label)
+			.then(function(response) {
+				dfd.resolve(response.Items);
+			});
+		return dfd.promise;
+	}
 
-    function _getAllPartNumbers(lookForThisPartialPartNumber) {
-    	var dfd = $q.defer();
-        OrderCloud.Me.ListProducts(null, 1, 20, null, null, {"Name": lookForThisPartialPartNumber+"*"})
-            .then(function(response) {
-	            if(Me.Org.xp.WeirGroup.label == "WVCUK") {
-		            partResults = response.Items;
-		            return OrderCloud.Me.ListProducts(null, 1, 20, null, null, {"xp.AlternatePartNumber":lookForThisPartialPartNumber+"*"})
-			            .then(function(altResponse) {
-				            partResults.push.apply(altResponse.Items);
-				            //return partResults;
-				            dfd.resolve(partResults);
-			            });
-	            } else {
-	            	//response.Items;
-		            dfd.resolve(response.Items);
-	            }
-            });
-	    return dfd.promise;
-    }
+	function _getAllPartNumbers(lookForThisPartialPartNumber) {
+		var dfd = $q.defer();
+		OrderCloud.Me.ListProducts(null, 1, 20, null, null, {"Name": lookForThisPartialPartNumber+"*"})
+			.then(function(response) {
+				if(Me.Org.xp.WeirGroup.label == "WVCUK") {
+					partResults = response.Items;
+					return OrderCloud.Me.ListProducts(null, 1, 20, null, null, {"xp.AlternatePartNumber":lookForThisPartialPartNumber+"*"})
+						.then(function(altResponse) {
+							partResults.push.apply(altResponse.Items);
+							//return partResults;
+							dfd.resolve(partResults);
+						});
+				} else {
+					//response.Items;
+					dfd.resolve(response.Items);
+				}
+			});
+		return dfd.promise;
+	}
 
-    //This method is used in the main search that is NOT the home page.
+	//This method is used in the main search that is NOT the home page.
 	function _getPart(lookForThisProduct, forThisCustomer) {
 		var filter = {
 			"s":{
@@ -234,25 +227,12 @@ function SearchProductsService($q, OrderCloud, Me, SearchTypeService) {
 					}
 				});
 		} else {
-			if(SearchTypeService.GetLastSearchType() == "s") {
-				return OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter[SearchTypeService.GetLastSearchType()], SearchTypeService.IsGlobalSearch() ? Me.Org.xp.WeirGroup.id == "1" ? null : "all" : null, Me.Org.xp.WeirGroup.label)
-					.then(function (response) {
-						return  OrderCloud.Me.ListCategories(lookForThisProduct, 1, 20, "Description", null, null , "all" , Me.Org.xp.WeirGroup.label)
-							.then(function(responseDescription){
-								var returnResults = response.Items.concat(responseDescription.Items);
-								returnResults = _.uniq(returnResults, false, function(cat){return cat.xp.SN});
-								return returnResults;
-							});
-					});
-			}
-			else {
-				return OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter[SearchTypeService.GetLastSearchType()], SearchTypeService.IsGlobalSearch() ? Me.Org.xp.WeirGroup.id == "1" ? null : "all" : null, Me.Org.xp.WeirGroup.label)
-					.then(function (response) {
-						return response.Items;
-					});
-			}
+			return OrderCloud.Me.ListCategories(null, 1, 20, null, null, filter[SearchTypeService.GetLastSearchType()], SearchTypeService.IsGlobalSearch() ? Me.Org.xp.WeirGroup.id=="1" ? null : "all" : null, Me.Org.xp.WeirGroup.label)
+				.then(function (response) {
+					return response.Items;
+				});
 		}
 	}
 
-    return service;
+	return service;
 }

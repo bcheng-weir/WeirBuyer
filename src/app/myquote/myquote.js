cf9ffb6ca9005b3ad28a23b600dc11207ad9440c
Ferguson <E054875@mcgladrey.rsm.net>
2018-03-01 14:05:25 -0600

    Update address listing to only show active addresses.

--- src/app/myquote/myquote.js
+++ src/app/myquote/myquote.js
@@ -244,7 +244,13 @@ function MyQuoteConfig($stateProvider) {
 			controllerAs: 'delivery',
 			resolve: {
 				Addresses: function(OrderCloudSDK, Me) {
-					return OrderCloudSDK.Addresses.List(Me.GetBuyerID());
+                    var f = {
+                        "xp.active":"true"
+                    };
+                    var opts = {
+                        filters: f
+                    };
+					return OrderCloudSDK.Addresses.List(Me.GetBuyerID(), opts);
 				}
 			}
 		})
